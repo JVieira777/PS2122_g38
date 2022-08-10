5c1dc0a07a89f14eb8db7d1c48e32436bf94f009
JosuePatatas <josue_zero@live.com.pt>
2022-08-10 19:59:59 +0100

    Userprofile Modal

---
+++ Frontend/my-app/src/Components/ExchangeViewModal.js
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from 'react'
+import '../Components/ExchangeViewModal.css'
+import axios from 'axios'
+import { RefundRequestModal } from '../Components/RefundRequestModal'
+import {DeleteExchange, GetBlockchainExchangeInfo} from '../pages/Exchange'
+import  { useWeb3Contract, useMoralis } from 'react-moralis'
+import abi from '../Constants/abi.json'
+import contractAddresses from '../Constants/contractAddress.json' 
+import  { useNavigate } from 'react-router-dom'
+
+
+export function ExchangeViewModal({exchange,setModal}){
+    const navigate = useNavigate()
+
+    const [product, setProduct] = useState(" ")
+
+    const [refundable,setRefundable] = useState(false)
+    const [pay,setPay] = useState(false)
+    const [collect,setCollect] = useState(false)
+    const [modalRefund,setModalRefund] = useState(false)
+
+
+
+
+    const { chainId: chainIdHex} = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const ExchangeAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    const {runContractFunction : collectRefund} = useWeb3Contract({
+        abi:abi,
+        contractAddress:ExchangeAddress,
+        functionName:"collectRefund",
+        params: {_id : exchange.id},
+    })
+
+
+
+
+
+
+
+    useEffect(() => {
+        const url = `http://localhost:8082/api/product/${exchange.pid}`
+        axios.get(url)
+            .then(response => {
+                setProduct(response.data)
+            })
+    }, [setProduct,product])
+    
+
+    useEffect(() => {
+        GetBlockchainExchangeInfo(exchange.id).then(response => {
+              
+            if(response.data.value6 && !response.data.value7){
+                setCollect(true)
+                
+             
+            }
+            else if(!response.data.value5){
+                setPay(true)
+                
+              
+            }
+            if(response.data.value5 && response.data.value7 && !response.data.value6){
+               setRefundable(true)
+               
+            }
+        })
+    }, [])
+  
+
+
+
+
+
+
+
+    return (
+        <div className='ModalBackground'>
+        <div className='Content'>
+        <h2><p>Exchange: {exchange.id}</p></h2>
+        <h2><p>name: {product.name}</p></h2>
+        <h2><p>description: {product.description}</p></h2>
+        <h2><p>Product Rating: {product.rate}</p></h2>
+        <h2><p>Totalprice: {(exchange.value)}</p></h2>
+    (<button onClick = {() => {
+        setModal(false)
+        }}
+        >
+        Close
+    </button>)
+    {refundable && RefundButton(setModalRefund)}
+    {pay && PayDeleteButtons(exchange)}
+    {collect && CollectRefundButton()}
+    {modalRefund && <RefundRequestModal exchange = {exchange}  setModal = {setModalRefund}/>}
+    </div>
+    
+    </div>
+    )
+    
+
+
+    function  RefundButton(setModalRefund){
+     return(
+        
+            <button onClick = {() => {
+            setModalRefund(true)
+            }}
+            >
+            Refund
+            </button>
+         
+    )
+    
+    }
+
+
+    function CollectRefundButton(){
+    return(
+        
+        <button onClick = {() => {
+        collectRefund()
+        }}
+        >
+        Collect
+        </button>
+   
+        )
+    }
+  
+
+
+    function PayDeleteButtons(exchange){
+            return(
+                <>
+            <button onClick = {() => {
+           navigate(`/product/payment/${exchange.id}`)
+            }}
+            >
+            pay
+            </button>
+
+            <button onClick = {() => {
+                DeleteExchange(exchange.id)
+                setModal(false)
+              }}
+            >
+            Cancel
+            </button>
+              </>
+        )
+
+    }
+}
